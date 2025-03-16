60f462dc947eefc4b913471a8c0d6ff48024bf7e
prajwalbagewadi <bagewadi.prajwal44@gmail.com>
2025-03-16 12:24:25 +0530

    interfaces types and values

--- 12_methods/meth.go
+++ 12_methods/meth.go
@@ -33,6 +33,36 @@ func (e Emp2) updateSal(newSal float64){
 	e.sal=newSal
 }
 
+
+func indentifyType(i interface{}){
+	//type switch
+	switch v := i.(type){
+	case string :
+		fmt.Println("this is a string",v)
+	case int :
+		fmt.Println("this is a integer",v)
+	case bool :
+		fmt.Println("this is a bool",v)
+	default :
+		fmt.Println("unknown type")	
+	}
+}
+
+//interface 
+type Speaker interface{
+	Speak() string
+}
+
+//struct
+type Person struct {
+	Name string
+}
+
+//interface method overriden
+func (p Person) Speak() string {
+	return "hello im "+p.Name
+}
+
 func main(){
 	fmt.Println("methods.")
 	var e1 = Emp{1,"abc","sde1",50000.00}
@@ -48,4 +78,34 @@ func main(){
 	e2.updateSal2(60000) //pointer as Receiver (pass by ref)
 	fmt.Println("e2=",e2)
 
+	//interface types 
+	//Type Assertion
+	var i interface{} = "helloworld"
+	str,ok := i.(string)
+
+	if ok {
+		fmt.Println("this is a string:",str)
+	} else {
+		fmt.Println("not a string.")
+	}
+
+	var i2 interface{} = 4
+	val,ok := i2.(string)
+	if ok {
+		fmt.Println("this is a string:",val)
+	} else {
+		fmt.Println("not a string")
+	}
+
+	//type switch call
+	indentifyType(20)
+	indentifyType("go")
+	indentifyType(true)
+
+	//interface values
+	var s Speaker //interface var
+	s = Person{"alice"} //assigning value to interface
+
+	fmt.Printf("Dynamic Type:%T\n",s)//prints concrete type
+	fmt.Printf("Dynamic value:",s.Speak())//func call
 }
